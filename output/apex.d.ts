8164f7bb59670018ef4e795a89376704ca2f0e35
Willie Ruemmele <willieruemmele@gmail.com>
2024-04-26 16:02:36 -0600

    feat: add generate script, .d.ts files, pre commit hook, GHA

---
+++ output/apex.d.ts
@@ -0,0 +1,333 @@
+export type FlowProcessType = 'AutoLaunchedFlow' 
+      | 'Flow' 
+      | 'Workflow' 
+      | 'CustomEvent' 
+      | 'InvocableProcess' 
+      | 'LoginFlow' 
+      | 'ActionPlan' 
+      | 'JourneyBuilderIntegration' 
+      | 'UserProvisioningFlow' 
+      | 'Survey' 
+      | 'SurveyEnrich' 
+      | 'Appointments' 
+      | 'FSCLending' 
+      | 'DigitalForm' 
+      | 'FieldServiceMobile' 
+      | 'OrchestrationFlow' 
+      | 'FieldServiceWeb' 
+      | 'TransactionSecurityFlow' 
+      | 'ContactRequestFlow' 
+      | 'ActionCadenceFlow' 
+      | 'ManagedContentFlow' 
+      | 'CheckoutFlow' 
+      | 'CartAsyncFlow' 
+      | 'DataCaptureFlow' 
+      | 'CustomerLifecycle' 
+      | 'Journey' 
+      | 'RecommendationStrategy' 
+      | 'Orchestrator' 
+      | 'RoutingFlow' 
+      | 'ServiceCatalogItemFlow' 
+      | 'EvaluationFlow' 
+      | 'LoyaltyManagementFlow' 
+      | 'ManagedContentAuthoringWorkflow' 
+      | 'ActionCadenceAutolaunchedFlow' 
+      | 'ActionCadenceStepFlow' 
+      | 'IndicatorResultFlow' 
+      | 'IndividualObjectLinkingFlow' 
+      | 'PromptFlow';
+
+export type PerfOption = 'NONE' 
+      | 'MINIMUM';
+
+export type LogCategory = 'Db' 
+      | 'Workflow' 
+      | 'Validation' 
+      | 'Callout' 
+      | 'Apex_code' 
+      | 'Apex_profiling' 
+      | 'Visualforce' 
+      | 'System' 
+      | 'Wave' 
+      | 'Nba' 
+      | 'All';
+
+export type LogCategoryLevel = 'None' 
+      | 'Finest' 
+      | 'Finer' 
+      | 'Fine' 
+      | 'Debug' 
+      | 'Info' 
+      | 'Warn' 
+      | 'Error';
+
+export type LogType = 'None' 
+      | 'Debugonly' 
+      | 'Db' 
+      | 'Profiling' 
+      | 'Callout' 
+      | 'Detail';
+
+export type ID = string;
+
+export type CompileAndTestRequest =  {
+    checkOnly: boolean;
+    classes?: string | string[];
+    deleteClasses?: string | string[];
+    deleteTriggers?: string | string[];
+    runTestsRequest?: RunTestsRequest;
+    triggers?: string | string[];
+}
+
+export type RunTestsRequest =  {
+    allTests: boolean;
+    classes?: string | string[];
+    maxFailedTests?: number;
+    namespace: string;
+    packages?: string | string[];
+    skipCodeCoverage?: boolean;
+    tests?: TestsNode | TestsNode[];
+}
+
+export type TestsNode =  {
+    classId: string;
+    className: string;
+    testMethods?: string | string[];
+}
+
+export type CompileAndTestResult =  {
+    classes?: CompileClassResult | CompileClassResult[];
+    deleteClasses?: DeleteApexResult | DeleteApexResult[];
+    deleteTriggers?: DeleteApexResult | DeleteApexResult[];
+    runTestsResult: RunTestsResult;
+    success: boolean;
+    triggers?: CompileTriggerResult | CompileTriggerResult[];
+}
+
+export type CompileClassResult =  {
+    bodyCrc?: number;
+    column: number;
+    id?: ID;
+    line: number;
+    name?: string;
+    problem?: string;
+    problems?: CompileIssue | CompileIssue[];
+    success: boolean;
+    warnings?: CompileIssue | CompileIssue[];
+}
+
+export type CompileIssue =  {
+    column?: number;
+    line?: number;
+    message?: string;
+}
+
+export type DeleteApexResult =  {
+    id?: ID;
+    problem?: string;
+    success: boolean;
+}
+
+export type RunTestsResult =  {
+    apexLogId?: string;
+    codeCoverage?: CodeCoverageResult | CodeCoverageResult[];
+    codeCoverageWarnings?: CodeCoverageWarning | CodeCoverageWarning[];
+    failures?: RunTestFailure | RunTestFailure[];
+    flowCoverage?: FlowCoverageResult | FlowCoverageResult[];
+    flowCoverageWarnings?: FlowCoverageWarning | FlowCoverageWarning[];
+    numFailures: number;
+    numTestsRun: number;
+    successes?: RunTestSuccess | RunTestSuccess[];
+    totalTime: number;
+}
+
+export type CodeCoverageResult =  {
+    id: ID;
+    locationsNotCovered?: CodeLocation | CodeLocation[];
+    name: string;
+    namespace?: string;
+    numLocations: number;
+    numLocationsNotCovered: number;
+    type: string;
+}
+
+export type CodeLocation =  {
+    column: number;
+    line: number;
+    numExecutions: number;
+    time: number;
+}
+
+export type CodeCoverageWarning =  {
+    id: ID;
+    message: string;
+    name?: string;
+    namespace?: string;
+}
+
+export type RunTestFailure =  {
+    id: ID;
+    message: string;
+    methodName?: string;
+    name: string;
+    namespace?: string;
+    seeAllData?: boolean;
+    stackTrace?: string;
+    time: number;
+    type: string;
+}
+
+export type FlowCoverageResult =  {
+    elementsNotCovered?: string | string[];
+    flowId: string;
+    flowName: string;
+    flowNamespace?: string;
+    numElements: number;
+    numElementsNotCovered: number;
+    processType: FlowProcessType;
+}
+
+export type FlowCoverageWarning =  {
+    flowId?: string;
+    flowName?: string;
+    flowNamespace?: string;
+    message: string;
+}
+
+export type RunTestSuccess =  {
+    id: ID;
+    methodName: string;
+    name: string;
+    namespace?: string;
+    seeAllData?: boolean;
+    time: number;
+}
+
+export type CompileTriggerResult =  {
+    bodyCrc?: number;
+    column: number;
+    id?: ID;
+    line: number;
+    name?: string;
+    problem?: string;
+    problems?: CompileIssue | CompileIssue[];
+    success: boolean;
+    warnings?: CompileIssue | CompileIssue[];
+}
+
+export type ExecuteAnonymousResult =  {
+    column: number;
+    compileProblem?: string;
+    compiled: boolean;
+    exceptionMessage?: string;
+    exceptionStackTrace?: string;
+    line: number;
+    success: boolean;
+}
+
+export type WsdlToApexInfo =  {
+    mapping?: NamespacePackagePair | NamespacePackagePair[];
+    wsdl: string;
+}
+
+export type NamespacePackagePair =  {
+    namespace: string;
+    packageName: string;
+}
+
+export type WsdlToApexResult =  {
+    apexScripts?: string | string[];
+    errors?: string | string[];
+    success: boolean;
+}
+
+export type LogInfo =  {
+    category: LogCategory;
+    level: LogCategoryLevel;
+}
+
+export type PackageVersion =  {
+    majorNumber: number;
+    minorNumber: number;
+    namespace: string;
+}
+
+export type AllowFieldTruncationHeader =  {
+    allowFieldTruncation: boolean;
+}
+
+export type CallOptions =  {
+    client: string;
+    perfOption: PerfOption;
+    uiRequestId: string;
+}
+
+export type DebuggingHeader =  {
+    categories?: LogInfo | LogInfo[];
+    debugLevel: LogType;
+}
+
+export type DebuggingInfo =  {
+    debugLog: string;
+}
+
+export type DisableFeedTrackingHeader =  {
+    disableFeedTracking: boolean;
+}
+
+export type PackageVersionHeader =  {
+    packageVersions?: PackageVersion | PackageVersion[];
+}
+
+export type SessionHeader =  {
+    sessionId: string;
+}
+
+export type compileAndTest =  {
+    CompileAndTestRequest: CompileAndTestRequest;
+}
+
+export type compileAndTestResponse =  {
+    result: CompileAndTestResult;
+}
+
+export type compileClasses =  {
+    scripts?: string | string[];
+}
+
+export type compileClassesResponse =  {
+    result?: CompileClassResult | CompileClassResult[];
+}
+
+export type compileTriggers =  {
+    scripts?: string | string[];
+}
+
+export type compileTriggersResponse =  {
+    result?: CompileTriggerResult | CompileTriggerResult[];
+}
+
+export type executeAnonymous =  {
+    String: string;
+}
+
+export type executeAnonymousResponse =  {
+    result: ExecuteAnonymousResult;
+}
+
+export type runTests =  {
+    RunTestsRequest: RunTestsRequest;
+}
+
+export type runTestsResponse =  {
+    result: RunTestsResult;
+}
+
+export type wsdlToApex =  {
+    info: WsdlToApexInfo;
+}
+
+export type wsdlToApexResponse =  {
+    result: WsdlToApexResult;
+}
+
---
+++ src/generate.ts
@@ -0,0 +1,395 @@
+import * as fs from 'fs';
+import {parseString} from 'xml2js';
+import type {
+    ComplexTypeNode,
+    ComplexTypeNodeWithComplexContent,
+    NodeWithAttributes,
+    SequenceNode,
+    SimpleTypeNode,
+    ComplexTypeNodeWithSequence,
+    ElementNode,
+    DefinitionsNode,
+    SchemaNode,
+} from './types';
+
+const wsdlFolder = './resources';
+const outputFolder = './output';
+
+const reservedWords = ['abstract',
+    'arguments',
+    'await',
+    'boolean',
+    'break',
+    'byte',
+    'case',
+    'catch',
+    'char',
+    'class',
+    'const',
+    'continue',
+    'debugger',
+    'default',
+    'delete',
+    'do',
+    'double',
+    'else',
+    'enum',
+    'eval',
+    'export',
+    'extends',
+    'false',
+    'final',
+    'finally',
+    'float',
+    'for',
+    'function',
+    'goto',
+    'if',
+    'implements',
+    'import',
+    'in',
+    'instanceof',
+    'int',
+    'interface',
+    'let',
+    'long',
+    'native',
+    'new',
+    'null',
+    'package',
+    'private',
+    'protected',
+    'public',
+    'return',
+    'short',
+    'static',
+    'super',
+    'switch',
+    'synchronized',
+    'this',
+    'throw',
+    'throws',
+    'transient',
+    'true',
+    'try',
+    'typeof',
+    'var',
+    'void',
+    'volatile',
+    'while',
+    'with',
+    'yield'];
+
+fs.readdirSync(wsdlFolder).forEach(
+    (wsdlFile) => {
+        console.dir(wsdlFile);
+
+        const fileName: string = wsdlFile.split('.')[0];
+        const extension: string = wsdlFile.split('.')[1];
+
+        if (extension === 'wsdl') {
+            const wsdl = fs.readFileSync(wsdlFolder + '/' + wsdlFile, 'utf-8');
+            const ts = convertWsdlToTypescript(wsdl);
+            fs.writeFileSync(outputFolder + '/' + fileName + '.d.ts', ts);
+        }
+    },
+);
+
+function convertWsdlToTypescript(wsdl: string): string {
+    let output = '';
+    parseString(wsdl
+            // Remove the xsd namespace prefix, because partner.wsdl does not use it.
+            .replaceAll('xsd:', '')
+            // Transform the namespace prefixes, to make them Typescript friendly.
+            .replaceAll('tns:', 'tns_')
+            .replaceAll('ens:', 'ens_')
+            .replaceAll('mns:', 'mns_')
+            .replaceAll('fns:', 'fns_')
+            .replaceAll('soap:', 'soap_')
+            .replaceAll('xmlns:', 'xmlns_'),
+        {
+            explicitArray: false,
+            mergeAttrs: false,
+            explicitChildren: false,
+            explicitCharkey: true,
+        },
+        (err, result) => {
+            toArray((result.definitions as DefinitionsNode).types.schema).forEach(
+
+                (schema) => {
+                    toArray(schema.simpleType).forEach(
+                        (simpleTypeNode: SimpleTypeNode) => {
+                            output += treatSimpleTypeNode(simpleTypeNode);
+                        },
+                    );
+
+                    toArray(schema.complexType).forEach(
+                        (complexTypeNode: ComplexTypeNode) => {
+                            output += treatComplexTypeNode(complexTypeNode);
+                        },
+                    );
+
+                    toArray(schema.element).forEach(
+                        (elementNode: ElementNode) => {
+                            output += treatElementNode(elementNode);
+                        },
+                    );
+                },
+            );
+        },
+    );
+
+    return output;
+}
+
+/*
+*********************
+* SIMPLE TYPE NODES *
+*********************
+
+* Simple types are either enums or primitives
+    * Example of a simple type as an enum:
+        <simpleType name="DeployProblemType">
+            <restriction base="string">
+                <enumeration value="Warning"/>
+                <enumeration value="Error"/>
+                <enumeration value="Info"/>
+            </restriction>
+        </simpleType>
+
+    * Examples of a simple type as a primitive, with and without additional restrictions, respectively:
+        <simpleType name="ID">
+            <restriction base="string">
+                <length value="18"/>
+                <pattern value="[a-zA-Z0-9]{18}"/>
+            </restriction>
+        </simpleType>
+        <simpleType name="PlatformSchemaContentType">
+            <restriction base="string"/>
+        </simpleType>
+
+* Note that restrictions like length and pattern are not taken into account while building the Typescript type.
+*/
+
+function treatSimpleTypeNode(simpleTypeNode: SimpleTypeNode): string {
+    let simpleNodeOutput = '';
+
+    const typeName: string = treatTypeName(simpleTypeNode.$.name || '');
+    const typePrimitive: string = simpleTypeNode.restriction.$.base || '';
+
+    if (typePrimitive === 'string') {
+        simpleNodeOutput += 'export type ' + typeName + ' = ';
+
+        const enumerations: NodeWithAttributes[] = simpleTypeNode.restriction.enumeration as NodeWithAttributes[];
+
+        if (enumerations && enumerations.length > 0) {
+            const enumerationValues: string[] = enumerations.map(
+                enumeration => enumeration.$.value || '',
+            );
+
+            simpleNodeOutput += '\'' + enumerationValues.join('\' \n      | \'') + '\'';
+        } else { // Not an enumeration
+            simpleNodeOutput += 'string';
+        }
+    } else {
+        simpleNodeOutput += 'export type ' + typeName + ' = ' + translateTypeName(typePrimitive);
+    }
+
+    simpleNodeOutput += ';\n\n';
+
+    return simpleNodeOutput;
+}
+
+/*
+**********************
+* COMPLEX TYPE NODES *
+**********************
+
+* Complex type nodes contain either a sequence block with a number of element nodes representing attributes or a complex content block.
+    * Example of a complex type node with a sequence:
+        <xsd:complexType name="CompileAndTestRequest">
+            <xsd:sequence>
+                <xsd:element name="checkOnly" type="xsd:boolean"/>
+                <xsd:element name="classes" minOccurs="0" maxOccurs="unbounded" type="xsd:string"/>
+                <xsd:element name="deleteClasses" minOccurs="0" maxOccurs="unbounded" type="xsd:string"/>
+                <xsd:element name="deleteTriggers" minOccurs="0" maxOccurs="unbounded" type="xsd:string"/>
+                <xsd:element name="runTestsRequest" minOccurs="0" type="tns:RunTestsRequest"/>
+                <xsd:element name="triggers" minOccurs="0" maxOccurs="unbounded" type="xsd:string"/>
+            </xsd:sequence>
+        </xsd:complexType>
+
+    * Example of a complex type node with a complex content:
+        <xsd:complexType name="AIApplication">
+            <xsd:complexContent>
+                <xsd:extension base="tns:Metadata">
+                    <xsd:sequence>
+                        <xsd:element name="developerName" type="xsd:string"/>
+                        <xsd:element name="masterLabel" minOccurs="0" type="xsd:string"/>
+                        <xsd:element name="predictionDefinitions" minOccurs="0" maxOccurs="unbounded" type="tns:AIPredictionDefinition"/>
+                        <xsd:element name="status" type="tns:AIApplicationStatus"/>
+                        <xsd:element name="type" type="tns:AIApplicationType"/>
+                    </xsd:sequence>
+                </xsd:extension>
+            </xsd:complexContent>
+        </xsd:complexType>
+*/
+
+function treatComplexTypeNode(complexTypeNode: ComplexTypeNode): string {
+    let complexNodeOutput = '';
+
+    const typeName = treatTypeName(complexTypeNode.$?.name ?? '');
+    let sequenceNode: SequenceNode | string | undefined;
+    let typeStructure: string;
+
+    if (complexTypeNode.complexContent) {
+        typeStructure = 'complexContent';
+    } else {
+        typeStructure = 'sequence';
+    }
+
+    complexNodeOutput += 'export type ' + typeName + ' = ';
+
+    switch (typeStructure) {
+        case 'complexContent': {
+            const updatedComplexTypeNode: ComplexTypeNodeWithComplexContent = complexTypeNode as ComplexTypeNodeWithComplexContent;
+            const parentType = updatedComplexTypeNode.complexContent.extension.$.base?.replace('tns_', '');
+            complexNodeOutput += parentType ? ' & = ' + treatTypeName(parentType) : '';
+            sequenceNode = updatedComplexTypeNode.complexContent.extension.sequence;
+            break;
+        }
+
+        case 'sequence': {
+            const updatedComplexTypeNode: ComplexTypeNodeWithSequence = complexTypeNode as ComplexTypeNodeWithSequence;
+            sequenceNode = updatedComplexTypeNode.sequence;
+            break;
+        }
+
+        default:
+    }
+
+    complexNodeOutput += ' {\n';
+
+    if (sequenceNode && sequenceNode !== '') {
+        toArray((sequenceNode as SequenceNode).element).forEach(
+            elementNode => {
+                complexNodeOutput += treatAttribute(elementNode);
+            },
+        );
+    }
+
+    complexNodeOutput += '}\n\n';
+
+    return complexNodeOutput;
+}
+
+function treatElementNode(elementNode: ElementNode): string {
+    let elementNodeOutput = '';
+
+    const typeName = treatTypeName(elementNode.$?.name);
+    const sequenceNode: SequenceNode | string = elementNode.complexType?.sequence ?? '';
+
+    elementNodeOutput += 'export type ' + typeName + ' = ';
+
+    elementNodeOutput += ' {\n';
+
+    if (sequenceNode && typeof sequenceNode !== 'string') {
+        toArray((sequenceNode).element).forEach(
+            subElementNode => {
+                elementNodeOutput += treatAttribute(subElementNode);
+            },
+        );
+    }
+
+    elementNodeOutput += '}\n\n';
+
+    return elementNodeOutput;
+}
+
+function treatAttribute(elementNode: NodeWithAttributes): string {
+    let attributeOutput = '';
+
+    const {name: fieldName, type: fieldTypeXml, minOccurs, maxOccurs, nillable} = elementNode.$;
+
+    const optional = (minOccurs === '0' || nillable === 'true') ? '?' : '';
+    attributeOutput += '    ' + fieldName + optional + ': ';
+
+    const fieldType = translateTypeName(fieldTypeXml ?? '') ;
+
+    if (maxOccurs === 'unbounded') {
+        attributeOutput += fieldType + ' | ' + fieldType + '[]';
+    } else {
+        attributeOutput += fieldType;
+    }
+
+    attributeOutput += ';\n';
+
+    return attributeOutput;
+}
+
+function translateTypeName(fieldTypeXml: string): string {
+    let fieldType = '';
+
+    switch (fieldTypeXml) {
+        case 'boolean': {
+            fieldType = 'boolean';
+            break;
+        }
+
+        case 'base64Binary':
+        case 'string': {
+            fieldType = 'string';
+            break;
+        }
+
+        case 'time':
+        case 'date':
+        case 'dateTime': {
+            fieldType = 'Date';
+            break;
+        }
+
+        case 'long':
+        case 'double':
+        case 'int': {
+            fieldType = 'number';
+            break;
+        }
+
+        case 'anyType': {
+            fieldType = 'any';
+            break;
+        }
+
+        default: {
+            if (fieldTypeXml?.startsWith('tns_')) {
+                fieldType = fieldTypeXml?.replace('tns_', '');
+            } else if (fieldTypeXml?.startsWith('ens_')) {
+                fieldType = fieldTypeXml?.replace('ens_', '');
+            } else if (fieldTypeXml?.startsWith('mns_')) {
+                fieldType = fieldTypeXml?.replace('mns_', '');
+            } else if (fieldTypeXml?.startsWith('fns_')) {
+                fieldType = fieldTypeXml?.replace('fns_', '');
+            } else {
+                fieldType = 'any';
+            }
+        }
+    }
+
+    return fieldType;
+}
+
+function treatTypeName(str: string): string {
+    if (reservedWords.includes(str)) {
+        str += '_';
+    }
+
+    return str;
+}
+
+function toArray<Type>(input: Type | Type[] | undefined): Type[] {
+    if (input === undefined) {
+        return [];
+    }
+
+    return Array.isArray(input) ? input : [input];
+}
