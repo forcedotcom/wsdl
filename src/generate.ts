9d164126f919e37a82b641ad56c907e7d4dc77a2
Willie Ruemmele <willieruemmele@gmail.com>
2024-04-30 14:23:25 -0600

    chore: use dev-scripts linting

--- src/generate.ts
+++ src/generate.ts
@@ -1,130 +1,137 @@
-import * as fs from 'fs';
-import {parseString, parseStringPromise} from 'xml2js';
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as fs from 'node:fs';
+import { parseString } from 'xml2js';
 import type {
-    ComplexTypeNode,
-    ComplexTypeNodeWithComplexContent,
-    NodeWithAttributes,
-    SequenceNode,
-    SimpleTypeNode,
-    ComplexTypeNodeWithSequence,
-    ElementNode,
-    DefinitionsNode,
+  ComplexTypeNode,
+  ComplexTypeNodeWithComplexContent,
+  NodeWithAttributes,
+  SequenceNode,
+  ComplexTypeNodeWithSequence,
+  DefinitionsNode,
+  SimpleTypeNode,
+  ElementNode,
 } from './types';
 
 const wsdlFolder = './resources';
 const outputFolder = './output';
 
-const reservedWords = ['abstract',
-    'arguments',
-    'await',
-    'boolean',
-    'break',
-    'byte',
-    'case',
-    'catch',
-    'char',
-    'class',
-    'const',
-    'continue',
-    'debugger',
-    'default',
-    'delete',
-    'do',
-    'double',
-    'else',
-    'enum',
-    'eval',
-    'export',
-    'extends',
-    'false',
-    'final',
-    'finally',
-    'float',
-    'for',
-    'function',
-    'goto',
-    'if',
-    'implements',
-    'import',
-    'in',
-    'instanceof',
-    'int',
-    'interface',
-    'let',
-    'long',
-    'native',
-    'new',
-    'null',
-    'package',
-    'private',
-    'protected',
-    'public',
-    'return',
-    'short',
-    'static',
-    'super',
-    'switch',
-    'synchronized',
-    'this',
-    'throw',
-    'throws',
-    'transient',
-    'true',
-    'try',
-    'typeof',
-    'var',
-    'void',
-    'volatile',
-    'while',
-    'with',
-    'yield'];
+const reservedWords = [
+  'abstract',
+  'arguments',
+  'await',
+  'boolean',
+  'break',
+  'byte',
+  'case',
+  'catch',
+  'char',
+  'class',
+  'const',
+  'continue',
+  'debugger',
+  'default',
+  'delete',
+  'do',
+  'double',
+  'else',
+  'enum',
+  'eval',
+  'export',
+  'extends',
+  'false',
+  'final',
+  'finally',
+  'float',
+  'for',
+  'function',
+  'goto',
+  'if',
+  'implements',
+  'import',
+  'in',
+  'instanceof',
+  'int',
+  'interface',
+  'let',
+  'long',
+  'native',
+  'new',
+  'null',
+  'package',
+  'private',
+  'protected',
+  'public',
+  'return',
+  'short',
+  'static',
+  'super',
+  'switch',
+  'synchronized',
+  'this',
+  'throw',
+  'throws',
+  'transient',
+  'true',
+  'try',
+  'typeof',
+  'var',
+  'void',
+  'volatile',
+  'while',
+  'with',
+  'yield',
+];
 
-fs.readdirSync(wsdlFolder).forEach(
-   async (wsdlFile) => {
-        console.dir(wsdlFile);
+fs.readdirSync(wsdlFolder).forEach((wsdlFile) => {
+  console.dir(wsdlFile);
 
-        const fileName = wsdlFile.split('.')[0];
-        const extension = wsdlFile.split('.')[1];
+  const fileName = wsdlFile.split('.')[0];
+  const extension = wsdlFile.split('.')[1];
 
-        if (extension === 'wsdl') {
-            const wsdl = fs.readFileSync(wsdlFolder + '/' + wsdlFile, 'utf-8');
-            const ts = convertWsdlToTypescript(wsdl);
-            fs.writeFileSync(outputFolder + '/' + fileName + '.d.ts', ts);
-        }
-    },
-);
+  if (extension === 'wsdl') {
+    const wsdl = fs.readFileSync(wsdlFolder + '/' + wsdlFile, 'utf-8');
+    const ts = convertWsdlToTypescript(wsdl);
+    fs.writeFileSync(outputFolder + '/' + fileName + '.d.ts', ts);
+  }
+});
 
 function convertWsdlToTypescript(wsdl: string): string {
-    let output = '';
+  let output = '';
 
-    parseString(wsdl
-            // Remove the xsd namespace prefix, because partner.wsdl does not use it.
-            .replaceAll('xsd:', '')
-            // Transform the namespace prefixes, to make them Typescript friendly.
-            .replaceAll('tns:', 'tns_')
-            .replaceAll('ens:', 'ens_')
-            .replaceAll('mns:', 'mns_')
-            .replaceAll('fns:', 'fns_')
-            .replaceAll('soap:', 'soap_')
-            .replaceAll('xmlns:', 'xmlns_'),
-        {
-            explicitArray: false,
-            mergeAttrs: false,
-            explicitChildren: false,
-            explicitCharkey: true,
-        },
-        (err, result) => {
-        const arr = toArray((result.definitions as DefinitionsNode).types.schema);
+  parseString(
+    wsdl
+      // Remove the xsd namespace prefix, because partner.wsdl does not use it.
+      .replaceAll('xsd:', '')
+      // Transform the namespace prefixes, to make them Typescript friendly.
+      .replaceAll('tns:', 'tns_')
+      .replaceAll('ens:', 'ens_')
+      .replaceAll('mns:', 'mns_')
+      .replaceAll('fns:', 'fns_')
+      .replaceAll('soap:', 'soap_')
+      .replaceAll('xmlns:', 'xmlns_'),
+    {
+      explicitArray: false,
+      mergeAttrs: false,
+      explicitChildren: false,
+      explicitCharkey: true,
+    },
+    (err, result) => {
+      const arr = toArray((result as { definitions: DefinitionsNode }).definitions.types.schema);
 
-        output += treatComplexTypeNode([
-            ...arr.flatMap(a => a.simpleType).filter(x=>x),
-            ...arr.flatMap(a => a.element).filter(x=>x),
-            ...arr.flatMap<ComplexTypeNode>(a => a.complexType).filter(x=>x)
-        ])
-        },
-    );
+      output += treatComplexTypeNode([
+        ...arr.flatMap((a) => a.simpleType).filter((x) => x),
+        ...arr.flatMap((a) => a.element).filter((x) => x),
+        ...arr.flatMap<ComplexTypeNode>((a) => a.complexType).filter((x) => x),
+      ]);
+    }
+  );
 
-    return output;
+  return output;
 }
 
 /*
@@ -190,167 +197,175 @@ function convertWsdlToTypescript(wsdl: string): string {
         </xsd:complexType>
 */
 
-function treatComplexTypeNode(complexTypeNodes: ComplexTypeNode[]): string {
-    let complexNodeOutput = '';
+function treatComplexTypeNode(source: Array<SimpleTypeNode | ComplexTypeNode | ElementNode>): string {
+  let complexNodeOutput = '';
 
-    const typeMap = new Map<string, {parents:string[], fields: SequenceNode[]}>();
+  const typeMap = new Map<string, { parents: string[]; fields: SequenceNode[] }>();
 
-    complexTypeNodes.map(type => {
-        const key = treatTypeName(type.$?.name??'')
-        if(!typeMap.has(key)){
-            if((type as ComplexTypeNodeWithComplexContent).complexContent){
-                // complex content case
-                const t = type as ComplexTypeNodeWithComplexContent;
-                typeMap.set(key, {parents: [t.complexContent.extension.$.base], fields: toArray(t.complexContent.extension.sequence?.element ?? []) as SequenceNode[]})
-            }else{
-                typeMap.set(key, {
-                    parents: [], fields: toArray((type as ComplexTypeNodeWithSequence).sequence?.element as SequenceNode[] ??
-                        // handle simple types here - adding enum values to fields
-                        // @ts-ignore
-                        toArray(type.restriction?.enumeration ?? []))
-                })
-            }
-        } else {
-            // we have this key, update it - use ! because we just ensured the map has it
-            const currValue = typeMap.get(key)!
-            if((type as ComplexTypeNodeWithComplexContent).complexContent){
-                // complex content case
-                const t = type as ComplexTypeNodeWithComplexContent;
-                currValue.parents.push(t.complexContent.extension.$.base)
-                currValue.fields.push(...toArray(t.complexContent.extension.sequence?.element) as SequenceNode[])
-            }else{
-                // @ts-ignore
-                currValue.fields.concat((type as ComplexTypeNodeWithSequence).sequence?.element ?? type.restriction.enumeration)
-            }
-            typeMap.set(key, currValue)
-        }
-    })
+  source.map((type) => {
+    const key = treatTypeName(type.$?.name ?? '');
+    if (!typeMap.has(key)) {
+      if ((type as ComplexTypeNodeWithComplexContent).complexContent) {
+        // complex content case
+        const t = type as ComplexTypeNodeWithComplexContent;
+        typeMap.set(key, {
+          parents: [t.complexContent.extension.$.base],
+          fields: toArray(t.complexContent.extension.sequence?.element ?? []) as SequenceNode[],
+        });
+      } else {
+        typeMap.set(key, {
+          parents: [],
+          fields: toArray(
+            ((type as ComplexTypeNodeWithSequence).sequence?.element as SequenceNode[]) ??
+              // handle simple types here - adding enum values to fields
+              toArray((type as SimpleTypeNode).restriction.enumeration ?? [])
+          ),
+        });
+      }
+    } else {
+      // we have this key, update it - use ! because we just ensured the map has it
+      const currValue = typeMap.get(key)!;
+      if ((type as ComplexTypeNodeWithComplexContent).complexContent) {
+        // complex content case
+        const t = type as ComplexTypeNodeWithComplexContent;
+        currValue.parents.push(t.complexContent.extension.$.base);
+        currValue.fields.push(...(toArray(t.complexContent.extension.sequence?.element) as SequenceNode[]));
+      } else {
+        currValue.fields.concat(
+          ((type as ComplexTypeNodeWithSequence).sequence?.element as SequenceNode[]) ??
+            (type as SimpleTypeNode).restriction.enumeration
+        );
+      }
+      typeMap.set(key, currValue);
+    }
+  });
 
-    typeMap.forEach((info, type)=>{
-        if(info.fields.every(f=> 'value' in f.$)){
-            // we have an enum type
-            complexNodeOutput+= 'export type ' + type + ' = '
-            if(info.fields.length===0) complexNodeOutput+='string'
-            complexNodeOutput+= info.fields.map(f => '\''+f.$.value+'\''  ).join('\n      |')
-            complexNodeOutput+='\n\n'
-        } else {
-            complexNodeOutput += 'export type ' + type + ' = '
-            complexNodeOutput += info.parents.map(p => treatTypeName(p)).join(' & ')
-            if(info.parents.length){
-                complexNodeOutput+= ' & {\n'
-            }else {
-                complexNodeOutput += '{\n'
-            }
-            // there's a duplciate field on UIObjectRelationConfig - with potentially more to come, filter out the duplicate
-            // as well as fields that shouldn't be here !('value' in n.$')
-            info.fields.filter((n, i) => info.fields.map(f=>f.$.name).indexOf(n.$.name)===i && !('value' in n.$) ).map(f => {
-                complexNodeOutput+= treatAttribute(f)
-            })
-            complexNodeOutput+= '}\n\n'
-        }
-    })
+  typeMap.forEach((info, type) => {
+    if (info.fields.every((f) => 'value' in f.$)) {
+      // we have an enum type
+      complexNodeOutput += 'export type ' + type + ' = ';
+      if (info.fields.length === 0) complexNodeOutput += 'string';
+      complexNodeOutput += info.fields.map((f) => "'" + f.$.value + "'").join('\n      |');
+      complexNodeOutput += '\n\n';
+    } else {
+      complexNodeOutput += 'export type ' + type + ' = ';
+      complexNodeOutput += info.parents.map((p) => treatTypeName(p)).join(' & ');
+      if (info.parents.length) {
+        complexNodeOutput += ' & {\n';
+      } else {
+        complexNodeOutput += '{\n';
+      }
+      // there's a duplciate field on UIObjectRelationConfig - with potentially more to come, filter out the duplicate
+      // as well as fields that shouldn't be here !('value' in n.$')
+      info.fields
+        .filter((n, i) => info.fields.map((f) => f.$.name).indexOf(n.$.name) === i && !('value' in n.$))
+        .map((f) => {
+          complexNodeOutput += treatAttribute(f);
+        });
+      complexNodeOutput += '}\n\n';
+    }
+  });
 
-    return complexNodeOutput
-
+  return complexNodeOutput;
 }
 
 function treatAttribute(elementNode: NodeWithAttributes): string {
-    let attributeOutput = '';
+  let attributeOutput = '';
 
-    const {name: fieldName, type: fieldTypeXml, minOccurs, maxOccurs, nillable} = elementNode.$;
+  const { name: fieldName, type: fieldTypeXml, minOccurs, maxOccurs, nillable } = elementNode.$;
 
-    console.assert(fieldName !== undefined, 'A field name is undefined', elementNode.$);
-    console.assert(fieldTypeXml !== undefined, 'A field type is undefined');
+  console.assert(fieldName !== undefined, 'A field name is undefined', elementNode.$);
+  console.assert(fieldTypeXml !== undefined, 'A field type is undefined');
 
-    const optional = (minOccurs === '0' || nillable === 'true') ? '?' : '';
-    attributeOutput += '    ' + fieldName + optional + ': ';
+  const optional = minOccurs === '0' || nillable === 'true' ? '?' : '';
+  attributeOutput += '    ' + fieldName + optional + ': ';
 
-    const fieldType = translateTypeName(fieldTypeXml ?? '') ?? 'any';
+  const fieldType = translateTypeName(fieldTypeXml ?? '') ?? 'any';
 
-    if (maxOccurs === 'unbounded') {
-        attributeOutput += fieldType + ' | ' + fieldType + '[]';
-    } else {
-        attributeOutput += fieldType;
-    }
+  if (maxOccurs === 'unbounded') {
+    attributeOutput += fieldType + ' | ' + fieldType + '[]';
+  } else {
+    attributeOutput += fieldType;
+  }
 
-    attributeOutput += ';\n';
+  attributeOutput += ';\n';
 
-    return attributeOutput;
+  return attributeOutput;
 }
 
 function translateTypeName(fieldTypeXml: string): string {
-    let fieldType = '';
+  let fieldType = '';
 
-    switch (fieldTypeXml) {
-        case 'boolean': {
-            fieldType = 'boolean';
-            break;
-        }
-        case 'base64Binary':
-        case 'string': {
-            fieldType = 'string';
-            break;
-        }
-
-        case 'double':
-        case 'long':
-        case 'int': {
-            fieldType = 'number';
-            break;
-        }
-
-        case 'dateTime':
-        case 'time':
-        case 'date': {
-            fieldType = 'Date';
-            break;
-        }
-
-        case 'anyType': {
-            fieldType = 'any';
-            break;
-        }
-
-        default: {
-            if (fieldTypeXml?.startsWith('tns_')) {
-                fieldType = fieldTypeXml?.replace('tns_', '');
-            } else if (fieldTypeXml?.startsWith('ens_')) {
-                fieldType = fieldTypeXml?.replace('ens_', '');
-            } else if (fieldTypeXml?.startsWith('mns_')) {
-                fieldType = fieldTypeXml?.replace('mns_', '');
-            } else if (fieldTypeXml?.startsWith('fns_')) {
-                fieldType = fieldTypeXml?.replace('fns_', '');
-            } else {
-                fieldType = 'any';
-            }
-        }
+  switch (fieldTypeXml) {
+    case 'boolean': {
+      fieldType = 'boolean';
+      break;
+    }
+    case 'base64Binary':
+    case 'string': {
+      fieldType = 'string';
+      break;
     }
 
-    return fieldType;
+    case 'double':
+    case 'long':
+    case 'int': {
+      fieldType = 'number';
+      break;
+    }
+
+    case 'dateTime':
+    case 'time':
+    case 'date': {
+      fieldType = 'Date';
+      break;
+    }
+
+    case 'anyType': {
+      fieldType = 'any';
+      break;
+    }
+
+    default: {
+      if (fieldTypeXml?.startsWith('tns_')) {
+        fieldType = fieldTypeXml?.replace('tns_', '');
+      } else if (fieldTypeXml?.startsWith('ens_')) {
+        fieldType = fieldTypeXml?.replace('ens_', '');
+      } else if (fieldTypeXml?.startsWith('mns_')) {
+        fieldType = fieldTypeXml?.replace('mns_', '');
+      } else if (fieldTypeXml?.startsWith('fns_')) {
+        fieldType = fieldTypeXml?.replace('fns_', '');
+      } else {
+        fieldType = 'any';
+      }
+    }
+  }
+
+  return fieldType;
 }
 
 function treatTypeName(str: string): string {
-    if (reservedWords.includes(str)) {
-        str += '_';
-    }
+  if (reservedWords.includes(str)) {
+    str += '_';
+  }
 
-    if (str?.startsWith('tns_')) {
-        str = str?.replace('tns_', '');
-    } else if (str?.startsWith('ens_')) {
-        str = str?.replace('ens_', '');
-    } else if (str?.startsWith('mns_')) {
-        str = str?.replace('mns_', '');
-    } else if (str?.startsWith('fns_')) {
-        str = str?.replace('fns_', '');
-    }
+  if (str?.startsWith('tns_')) {
+    str = str?.replace('tns_', '');
+  } else if (str?.startsWith('ens_')) {
+    str = str?.replace('ens_', '');
+  } else if (str?.startsWith('mns_')) {
+    str = str?.replace('mns_', '');
+  } else if (str?.startsWith('fns_')) {
+    str = str?.replace('fns_', '');
+  }
 
-    return str;
+  return str;
 }
 
 function toArray<Type>(input: Type | Type[] | undefined): Type[] {
-    if (input === undefined) {
-        return [];
-    }
+  if (input === undefined) {
+    return [];
+  }
 
-    return Array.isArray(input) ? input : [input];
+  return Array.isArray(input) ? input : [input];
 }
--- src/types.ts
+++ src/types.ts
@@ -1,61 +1,63 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 export type Properties = Record<string, string>;
 
 export type Node = {
-    $?: Properties;
+  $?: Properties;
 };
 
 export type NodeWithAttributes = {
-    $: Properties;
+  $: Properties;
 } & Node;
 
 export type SequenceNode = {
-    element: NodeWithAttributes[] | NodeWithAttributes;
+  element: NodeWithAttributes[] | NodeWithAttributes;
 } & NodeWithAttributes;
 
 export type ExtensionNode = {
-    sequence: SequenceNode;
+  sequence: SequenceNode;
 } & NodeWithAttributes;
 
 export type ComplexContentNode = {
-    extension: ExtensionNode;
+  extension: ExtensionNode;
 } & Node;
 
 export type ComplexTypeNode = Record<string, unknown> & Node;
 
 export type ComplexTypeNodeWithSequence = {
-    sequence?: SequenceNode;
+  sequence?: SequenceNode;
 } & ComplexTypeNode;
 
 export type ComplexTypeNodeWithComplexContent = {
-    complexContent: ComplexContentNode;
+  complexContent: ComplexContentNode;
 } & ComplexTypeNode;
 
 export type RestrictionNode = {
-    enumeration: NodeWithAttributes[] | NodeWithAttributes;
+  enumeration: NodeWithAttributes[] | NodeWithAttributes;
 } & NodeWithAttributes;
 
 export type SimpleTypeNode = {
-    restriction: RestrictionNode;
+  restriction: RestrictionNode;
 } & NodeWithAttributes;
 
 export type ElementNode = {
-    complexType: ComplexTypeNodeWithSequence;
+  complexType: ComplexTypeNodeWithSequence;
 } & NodeWithAttributes;
 
 export type SchemaNode = {
-    complexType: ComplexTypeNode[] | ComplexTypeNode;
-    simpleType: SimpleTypeNode[] | SimpleTypeNode;
-    element: ElementNode[] | ElementNode;
+  complexType: ComplexTypeNode[] | ComplexTypeNode;
+  simpleType: SimpleTypeNode[] | SimpleTypeNode;
+  element: ElementNode[] | ElementNode;
 } & NodeWithAttributes;
 
 export type TypesNode = {
-    schema: SchemaNode | SchemaNode[];
+  schema: SchemaNode | SchemaNode[];
 } & Node;
 
 export type DefinitionsNode = {
-    types: TypesNode;
-    message: any;
-    portType: any;
-    binding: any;
-    service: any;
+  types: TypesNode;
 } & NodeWithAttributes;
